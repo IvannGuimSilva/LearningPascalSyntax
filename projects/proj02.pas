a7079748823d1fb35e6428726d7ad9ab9fe8752b
IvannGuimSilva <ivanguimsilva2504@gmail.com>
2023-03-11 01:26:33 -0300

    Testing commit

--- projects/proj02.pas
+++ projects/proj02.pas
@@ -1,8 +1,22 @@
-Program Programa02; // Cabeçalho
+Program Programa02; // Cabeçalho  - Nome Programa.
 	{
 		Seção de definição e declaração
 		de dados.
 	}
+	
+	const a: integer = 26;
+				b: char = 'I';
+				c: boolean = true;
+				d: real = 3.141592;
+				e: string = 'Pascalzim';
+				
+	
 Begin
 	{ Seção de comandos. }
+	
+	writeln(a);
+	writeln(b);
+	writeln(c);
+	writeln(d);
+	writeln(e);
 End.
